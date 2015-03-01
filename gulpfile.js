58393011daae041de80d2c0fdc92d360b7452d91
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-03-02 00:34:40 +0200

    added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
+'use strict';
+
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-  
+	
+});
+
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+    gulp.src(['./app/**/*.js'])
+    .pipe(concat('script.js'))
+    //.pipe(stripDebug())
+    //.pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
 });
