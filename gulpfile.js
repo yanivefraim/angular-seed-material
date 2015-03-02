cf904d032b6424ab8c35bb17ed85de3b31100c57
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-03-02 20:30:02 +0200

    gulp ngAnnotate

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-
+var ngAnnotate = require('gulp-ng-annotate');
 gulp.task('default', function() {
 	
 });
@@ -10,6 +10,7 @@ gulp.task('default', function() {
 // JS concat, strip debugging and minify
 gulp.task('scripts', function() {
     gulp.src(['./app/**/*.js'])
+    .pipe(ngAnnotate())
     .pipe(concat('script.js'))
     //.pipe(stripDebug())
     //.pipe(uglify())
